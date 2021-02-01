f359b0a8ec0fa4222f87d59eb350f3f0e8e11441
mrclfd <mrclfd@yahoo.com>
2021-02-02 02:54:31 +0700

    Create android.sh

---
+++ string-darkprojekt/android.sh
@@ -0,0 +1,8 @@
+apt update
+apt install nodejs --fix-missing
+pkg install git
+git clone https://github.com/mrclfd/DarkProjekt
+cd DarkProjekt
+npm install @adiwajshing/baileys
+npm install chalk
+node qr.js
